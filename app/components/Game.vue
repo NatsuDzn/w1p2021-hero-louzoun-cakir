77579f9c8690fb47dab3a25e3992004fe9d613c8
Nathanael <natsuxgraph@gmail.com>
2019-05-23 13:31:32 +0200

    adapt  game layout

--- app/components/Game.vue
+++ app/components/Game.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="game">
-    <router-link to="/">
+<div class="game_buttons">
+ <router-link to="/">
       <div class="game__backHome"><img src="../assets/images/icons/home.svg"/></div>
     </router-link>
     <div class="game__mute" onclick="document.getElementById('music').pause()" ><img src="../assets/images/icons/volume_off.svg"/></div>
     <div class="game__unmute" onclick="document.getElementById('music').play()" ><img src="../assets/images/icons/volume_on.svg"/></div>
+  <div class="game">
     <audio id="music" src="../assets/audios/theme.mp3" autoplay loop></audio>
     <img :src="step.background" :alt="step.background" class="game__image">
     <h1 v-html="step.title" class="game__title">{{step.title}}</h1>
