b3e3b4e157022f481b88145217379611dfabb826
HETIC <hetic@MacBook-Pro-de-Remi-9.local>
2019-05-24 18:33:44 +0200

    player name

--- app/components/Characters.vue
+++ app/components/Characters.vue
@@ -6,9 +6,8 @@
     <router-link to="/game/3">
       <img class="sprites" src="../assets/images/sprites/Kirua.png">
     </router-link>
-    <router-link to="/intro">
-      <img class="sprites" src="../assets/images/sprites/You.png">
-    </router-link>
+    <input v-model="name" v-on:keyup.enter="registerName" placeholder="Entrez votre nom">
+
     <router-link to="/game/3">
       <img class="sprites" src="../assets/images/sprites/Kurapika.png">
     </router-link>
@@ -17,3 +16,19 @@
     </router-link>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: ""
+      };
+    },
+    methods: {
+      registerName() {
+        localStorage.setItem('playerName', this.name)
+        window.location.href = 'http://localhost:1234/intro'
+      }
+    }
+  };
+</script>
--- app/components/Lose.vue
+++ app/components/Lose.vue
@@ -4,10 +4,21 @@
     <div class="lose__unmute" onclick="document.getElementById('music').play()" ><img src="../assets/images/icons/volume_on.svg"/></div>
     <!-- Peut être changer le son mdr -->
     <audio id="music" src="../assets/audios/lose.mp3" autoplay loop></audio>
+    <h1 v-if="name" class="win__title">Dommage {{name}},</h1>
     <h1 class="lose__title">Vous avez échoué à l'examen</h1>
     <img class="lose__image" src="../assets/images/take-the-l.jpg">
     <router-link to="/" class="lose__link">
-      <span @click="reset">Retenter de devenir un hunter</span>
+      <span>Retenter de devenir un hunter</span>
     </router-link>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: localStorage.getItem('playerName')
+      };
+    }
+  };
+</script>
